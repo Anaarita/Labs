1cb2843ac7efd84ee5323355c3b12fe76c715946
Anaarita <112184415+Anaarita@users.noreply.github.com>
2022-09-12 22:21:32 -0600

    Add files via upload

---
+++ TEA2/lab02/lab02-oyo.py
@@ -0,0 +1,22 @@
+# Tendencias e Innovacion en Tecnologia Agricola
+horas = 10 
+minutos = 15
+
+# calculando el total de minutos
+total = horas * 60 + minutos
+
+#display total:
+print(total)
+
+#display total:
+print(type(total))
+
+#display exponenciacion:
+
+otroCalculo = 2 ** 3 + (6-10)/10
+
+print(otroCalculo)
+
+# remainder (sobrante)
+sobrante = 7 % 3
+print(sobrante)
